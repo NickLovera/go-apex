a3173fc5635ae2b56e47f2250e4fa0aebb21cdd1
Nick Lovera <nick.lovera2@gmail.com>
2021-06-14 01:22:54 -0500

    Please Work

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	menu "github.com/NickLovera/go-apex/Menu"
+	logic "github.com/NickLovera/go-apex/Mgr"
+	"time"
+)
+
+var squadStats = logic.GetStats()
+var lastUpdate = time.Now()
+
+func main() {
+
+	for {
+		menu.PrintMenu(lastUpdate)
+		choice := menu.GetChoice()
+
+		if time.Since(lastUpdate).Minutes() > 5 {
+			fmt.Println("Retrieving Stats......")
+			squadStats = logic.GetStats()
+			lastUpdate = time.Now()
+		}
+
+		switch choice {
+		case 1:
+			getEveryone()
+		case 2:
+			fmt.Println("Who would you like to view?\n" +
+				"1. Hk_Dingledorf\n2. Its_SkeetR\n3. MoneyManRex937\n4. SourMonkeyy")
+			playerId := menu.GetChoice()
+			getIndiv(playerId)
+		}
+	}
+}
+
+func getEveryone() {
+	logic.GetEveryone(squadStats)
+}
+
+func getIndiv(playerId int) {
+	logic.GetIndivdual(squadStats, playerId)
+}
